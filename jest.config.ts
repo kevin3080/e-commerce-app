1204409254de41fce12516a8dd8976b71bbd8b70
kevin <kevinpernia29@gmail.com>
2025-06-17 23:29:33 -0500

    migrate from Babel to ts-jest and remove babel config

---
+++ jest.config.ts
@@ -0,0 +1,25 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleNameMapper: {
+    '^.+\\.svg$': 'jest-svg-transformer',
+    '^.+\\.(css|less|scss)$': 'identity-obj-proxy',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/app/__mocks__/fileMock.ts'
+  },
+  testMatch: [
+    '**/__tests__/**/*.+(ts|tsx|js)',
+    '**/?(*.)+(spec|test).+(ts|tsx|js)'
+  ],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  transform: {
+  '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: 'tsconfig.app.json' }]
+  },
+};
+
+export default config;
